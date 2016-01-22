8451a70def7e364aa9a39d169a102d8a9bc605eb
Copper Phosphate <cu3po42@gmail.com>
2016-01-22 15:40:29 +0100

    Fix building

--- server.d.ts
+++ server.d.ts
@@ -1,3 +1,1 @@
-/// <reference path="typings/node/node.d.ts" />
-/// <reference path="typings/github-electron/github-electron.d.ts" />
 export default function register(message: string, fn: (...args: any[]) => Promise<any>): void;
